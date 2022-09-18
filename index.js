b2ecdbcd65b30e5506e1e6b66df063b3ef7a3648
mayurisamanta <samantamayuri@gmail.com>
2022-09-18 17:29:13 +0530

    almost done

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
